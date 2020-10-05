a040b51b735436f5b49bba01e069b3487b33891b
tuankhac <tuankhac19594@gmail.com>
2020-10-05 18:49:06 +0700

    merge code

--- src/main/java/com/neo/nbdapi/dao/impl/PaginationDAOImpl.java
+++ src/main/java/com/neo/nbdapi/dao/impl/PaginationDAOImpl.java
@@ -8,8 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Repository
 public class PaginationDAOImpl implements PaginationDAO {
@@ -19,44 +17,48 @@ public class PaginationDAOImpl implements PaginationDAO {
 	@Autowired
 	private HikariDataSource ds;
 
-    /**
-     * Get result paging
-     * @param sql
-     * @param pageNumber
-     * @param recordPerPage
-     * @param parameter
-     * @return
-     * @throws SQLException
-     */
-    @Override
-    public ResultSet getResultPagination(Connection connection, String sql, int pageNumber, int recordPerPage, Object... parameter) throws SQLException {
-        if (recordPerPage > 0) {
-                    StringBuilder sqlPagination = new StringBuilder("");
-                    if (pageNumber < 2) {
-                        sqlPagination.append("select rownum stt, xlpt.* from (");
-                        sqlPagination.append(sql);
-                        sqlPagination.append(") xlpt where rownum < ");
-                        sqlPagination.append(recordPerPage + 1);
-                    } else {
-                        sqlPagination.append("select /*+ first_rows(");
-                        sqlPagination.append(recordPerPage);
-                        sqlPagination.append(") */ xlpt.* from (select rownum row_stt, xlpt.* from (");
-                        sqlPagination.append(sql);
-                        sqlPagination.append(") xlpt where rownum <= ");
-                        sqlPagination.append(pageNumber * recordPerPage);
-                        sqlPagination.append(" ) xlpt where row_stt > ");
-                        sqlPagination.append(((pageNumber - 1) * recordPerPage));
+	/**
+	 * Get result paging
+	 * 
+	 * @param sql
+	 * @param pageNumber
+	 * @param recordPerPage
+	 * @param parameter
+	 * @return
+	 * @throws SQLException
+	 */
+	@Override
+	public ResultSet getResultPagination(String sql, int pageNumber, int recordPerPage, Object... parameter)
+			throws SQLException {
+		if (recordPerPage > 0) {
+			try (Connection connection = ds.getConnection()) {
+				StringBuilder sqlPagination = new StringBuilder("");
+				if (pageNumber < 2) {
+					sqlPagination.append("select rownum stt, xlpt.* from (");
+					sqlPagination.append(sql);
+					sqlPagination.append(") xlpt where rownum < ");
+					sqlPagination.append(recordPerPage + 1);
+				} else {
+					sqlPagination.append("select /*+ first_rows(");
+					sqlPagination.append(recordPerPage);
+					sqlPagination.append(") */ xlpt.* from (select rownum row_stt, xlpt.* from (");
+					sqlPagination.append(sql);
+					sqlPagination.append(") xlpt where rownum <= ");
+					sqlPagination.append(pageNumber * recordPerPage);
+					sqlPagination.append(" ) xlpt where row_stt > ");
+					sqlPagination.append(((pageNumber - 1) * recordPerPage));
 
-                    }
-                    logger.debug("JDBC execute query: {}", sqlPagination);
-                    PreparedStatement statement = connection.prepareStatement(sqlPagination.toString(), ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-                    for (int i = 0; i < parameter.length; i++) {
-                        statement.setObject(i + 1, parameter[i]);
-                    }
-                    return statement.executeQuery();
-            }
-        return null;
-    }
+				}
+				logger.debug("JDBC execute query: {}", sqlPagination);
+				PreparedStatement statement = connection.prepareStatement(sqlPagination.toString());
+				for (int i = 0; i < parameter.length; i++) {
+					statement.setObject(i + 1, parameter[i]);
+				}
+				return statement.executeQuery();
+			}
+		}
+		return null;
+	}
 
 	/**
 	 * count all result query of pagination
