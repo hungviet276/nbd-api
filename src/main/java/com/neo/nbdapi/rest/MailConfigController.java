8769d615506547d946b40e2a26632427eb91c00c
tuankhac <tuankhac19594@gmail.com>
2020-10-06 13:51:45 +0700

    ignore de chay authen

--- src/main/java/com/neo/nbdapi/rest/MailConfigController.java
+++ src/main/java/com/neo/nbdapi/rest/MailConfigController.java
@@ -1,35 +1,35 @@
-package com.neo.nbdapi.rest;
-
-import com.neo.nbdapi.dto.DefaultPaginationDTO;
-import com.neo.nbdapi.dto.DefaultResponseDTO;
-import com.neo.nbdapi.exception.BusinessException;
-import com.neo.nbdapi.rest.vm.CreateMailConfigVM;
-import com.neo.nbdapi.rest.vm.DefaultRequestPagingVM;
-import com.neo.nbdapi.services.MailConfigService;
-import com.neo.nbdapi.utils.Constants;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
-import java.sql.SQLException;
-
-@RestController
-@RequestMapping(Constants.APPLICATION_API.API_PREFIX + Constants.APPLICATION_API.MODULE.URI_MAIL_CONFIG)
-public class MailConfigController {
-
-    @Autowired
-    private MailConfigService mailConfigService;
-
-    @PostMapping("/get-list-mail-config-pagination")
-    public DefaultPaginationDTO getListMailConfigPagination(@RequestBody @Valid DefaultRequestPagingVM defaultRequestPagingVM) throws SQLException, BusinessException {
-        return mailConfigService.getListMailConfigPagination(defaultRequestPagingVM);
-    }
-
-    @PostMapping("/create-mail-config")
-    public DefaultResponseDTO createMailConfig(@RequestBody @Valid CreateMailConfigVM createMailConfigVM) throws SQLException {
-        return mailConfigService.createMailConfig(createMailConfigVM);
-    }
-}
+//package com.neo.nbdapi.rest;
+//
+//import com.neo.nbdapi.dto.DefaultPaginationDTO;
+//import com.neo.nbdapi.dto.DefaultResponseDTO;
+//import com.neo.nbdapi.exception.BusinessException;
+//import com.neo.nbdapi.rest.vm.CreateMailConfigVM;
+//import com.neo.nbdapi.rest.vm.DefaultRequestPagingVM;
+//import com.neo.nbdapi.services.MailConfigService;
+//import com.neo.nbdapi.utils.Constants;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import javax.validation.Valid;
+//import java.sql.SQLException;
+//
+//@RestController
+//@RequestMapping(Constants.APPLICATION_API.API_PREFIX + Constants.APPLICATION_API.MODULE.URI_MAIL_CONFIG)
+//public class MailConfigController {
+//
+//    @Autowired
+//    private MailConfigService mailConfigService;
+//
+//    @PostMapping("/get-list-mail-config-pagination")
+//    public DefaultPaginationDTO getListMailConfigPagination(@RequestBody @Valid DefaultRequestPagingVM defaultRequestPagingVM) throws SQLException, BusinessException {
+//        return mailConfigService.getListMailConfigPagination(defaultRequestPagingVM);
+//    }
+//
+//    @PostMapping("/create-mail-config")
+//    public DefaultResponseDTO createMailConfig(@RequestBody @Valid CreateMailConfigVM createMailConfigVM) throws SQLException {
+//        return mailConfigService.createMailConfig(createMailConfigVM);
+//    }
+//}
