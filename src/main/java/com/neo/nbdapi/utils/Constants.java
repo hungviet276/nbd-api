2301e9bd8377dcf884100e576e31b17a31b6d1a3
Hauvt <hauhanh1998@gmail.com>
2020-10-28 11:59:57 +0700

    update

--- src/main/java/com/neo/nbdapi/utils/Constants.java
+++ src/main/java/com/neo/nbdapi/utils/Constants.java
@@ -18,7 +18,7 @@ public interface Constants {
             public static final String URI_CONFIG_VALUE_TYPES = "/config-value-type";
             public static final String URI_CONFIG_WARNING_THRESOLD = "/warning-thresold";
             public static final String URI_CHANGER_PASS = "/changer-pass";
-
+        }
     }
 
     class EXCEPTION {
