464ff3009b1317fda62643bf6dcc1bbf87ccfdc7
Leducanh123456 <forrestgump.nxl@gmail.com>
2020-11-02 15:42:21 +0700

    add ck editor

---
+++ src/main/java/com/neo/nbdapi/rest/WarningManagerStationController.java
@@ -0,0 +1,10 @@
+package com.neo.nbdapi.rest;
+
+import com.neo.nbdapi.utils.Constants;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(Constants.APPLICATION_API.API_PREFIX + Constants.APPLICATION_API.MODULE.URI_CONFIG_WARNING_MANAGER_STATION)
+public class WarningManagerStationController {
+}
