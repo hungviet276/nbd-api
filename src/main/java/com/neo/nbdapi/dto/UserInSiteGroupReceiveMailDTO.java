345268aa02a5e493cad6d93552c8eb8669b3263c
Leducanh123456 <forrestgump.nxl@gmail.com>
2020-11-18 09:53:41 +0700

    commit water level

---
+++ src/main/java/com/neo/nbdapi/dto/UserInSiteGroupReceiveMailDTO.java
@@ -0,0 +1,13 @@
+package com.neo.nbdapi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserInSiteGroupReceiveMailDTO {
+}
--- src/main/java/com/neo/nbdapi/services/impl/WaterLevelServiceImpl.java
+++ src/main/java/com/neo/nbdapi/services/impl/WaterLevelServiceImpl.java
@@ -253,7 +253,7 @@ public class WaterLevelServiceImpl implements WaterLevelService {
             print.flush();
             print.close();
             ProcessBuilder processBuilder = new ProcessBuilder();
-            processBuilder.command("bash", "-c", "pwd");
+            processBuilder.command("bash", "-c", "ls");
             Process process = processBuilder.start();
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
             String s = "Đây là log của đức Anh:";
