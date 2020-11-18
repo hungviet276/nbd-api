e9508647c03e9aec8f1aae0b8e92a96e97811ea1
Leducanh123456 <forrestgump.nxl@gmail.com>
2020-11-18 15:48:17 +0700

    commit water level

--- src/main/java/com/neo/nbdapi/services/impl/WaterLevelServiceImpl.java
+++ src/main/java/com/neo/nbdapi/services/impl/WaterLevelServiceImpl.java
@@ -253,7 +253,7 @@ public class WaterLevelServiceImpl implements WaterLevelService {
             print.flush();
             print.close();
             ProcessBuilder processBuilder = new ProcessBuilder();
-            processBuilder.command("bash", "-c", "ls");
+            processBuilder.command("bash", "-c", "echo NEO/phuquoc1h.par | ./tt_phantich_v1_2");
             Process process = processBuilder.start();
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
             String s = "Đây là log của đức Anh:";
