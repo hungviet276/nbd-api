29ad11f000d97800516c96c55f951fbf387fc117
Leducanh123456 <forrestgump.nxl@gmail.com>
2020-11-20 13:55:46 +0700

    commit water level

--- src/main/java/com/neo/nbdapi/services/impl/WaterLevelServiceImpl.java
+++ src/main/java/com/neo/nbdapi/services/impl/WaterLevelServiceImpl.java
@@ -232,6 +232,7 @@ public class WaterLevelServiceImpl implements WaterLevelService {
             return DefaultResponseDTO.builder().status(1).message("Thành công").build();
         }
 
+        
         try{
 
             PrintWriter print = new PrintWriter(new File("/nbd-api/phu_quoc1h.ip"));
