cc2a1cbfe0981d42e1a9a9f70562c3096362db98
Leducanh123456 <forrestgump.nxl@gmail.com>
2020-12-02 16:56:15 +0700

    commit water_level

--- src/main/java/com/neo/nbdapi/entity/ConfigValueType.java
+++ src/main/java/com/neo/nbdapi/entity/ConfigValueType.java
@@ -1,5 +1,6 @@
 package com.neo.nbdapi.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,7 +21,9 @@ public class ConfigValueType {
     private Float max;
     private Float variableTime;
     private Float variableSpatial;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss",timezone = "Asia/Ho_Chi_Minh")
     private Date startDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss",timezone = "Asia/Ho_Chi_Minh")
     private Date endDate;
     private String code;
 }
