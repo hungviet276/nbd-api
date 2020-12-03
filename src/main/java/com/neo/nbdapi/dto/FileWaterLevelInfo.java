39598e7722f75ec9efd438a324c92ed00aaa3a2e
Leducanh123456 <forrestgump.nxl@gmail.com>
2020-12-03 16:41:17 +0700

    commit water_level

--- src/main/java/com/neo/nbdapi/dto/FileWaterLevelInfo.java
+++ src/main/java/com/neo/nbdapi/dto/FileWaterLevelInfo.java
@@ -1,5 +1,6 @@
 package com.neo.nbdapi.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,5 +13,6 @@ import java.util.Date;
 @Builder
 public class FileWaterLevelInfo implements Serializable {
     private String fileName;
-    private String modifyDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss",timezone = "Asia/Ho_Chi_Minh")
+    private Date modifyDate;
 }
